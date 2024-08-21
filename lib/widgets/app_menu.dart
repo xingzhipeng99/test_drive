e3f4ea989b0440d665ed962cb70dd07a98d7328e
ZhiPeng Xing <zhipeng.xing@mt.com>
2024-08-21 09:30:43 +0800

    change app name add fontawsome icon

--- lib/widgets/app_menu.dart
+++ lib/widgets/app_menu.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:test_drive/screens/about.dart';
 import 'package:test_drive/screens/albums.dart';
 import 'package:test_drive/screens/cart_screen.dart';
@@ -22,51 +23,81 @@ class AppMenu extends StatelessWidget {
           ),
           child: ListTile(
             onTap: () => Navigator.popAndPushNamed(context, '/profile'),
-            leading: const Icon(Icons.account_box),
-            title: const Text('Admin'),
+            leading: const Icon(
+              Icons.account_box,
+              color: Colors.white,
+            ),
+            title: const Text(
+              'Admin',
+              style: TextStyle(color: Colors.white),
+            ),
           ),
         ),
         ListTile(
           onTap: () => Navigator.popAndPushNamed(context, '/'),
-          leading: const Icon(Icons.home),
+          leading: const Icon(
+            Icons.home,
+            color: Colors.blue,
+          ),
           title: const Text('Home'),
         ),
         ListTile(
           onTap: () =>
               Navigator.popAndPushNamed(context, ProductsPage.routeName),
-          leading: const Icon(Icons.card_giftcard),
+          leading: const Icon(
+            Icons.card_giftcard,
+            color: Colors.blue,
+          ),
           title: const Text('Products'),
         ),
         ListTile(
           onTap: () => {Navigator.popAndPushNamed(context, CartPage.routeName)},
-          leading: const Icon(Icons.shopping_cart),
+          leading: const Icon(
+            Icons.shopping_cart,
+            color: Colors.blue,
+          ),
           title: const Text('Cart'),
         ),
         ListTile(
           onTap: () =>
               Navigator.popAndPushNamed(context, LocationsPage.routeName),
-          leading: const Icon(Icons.location_on),
+          leading: const FaIcon(
+            FontAwesomeIcons.locationArrow,
+            color: Colors.blue,
+          ),
           title: const Text('Locations'),
         ),
         ListTile(
           onTap: () => Navigator.popAndPushNamed(context, AlbumsPage.routeName),
-          leading: const Icon(Icons.photo_album),
+          leading: const Icon(
+            Icons.photo_album,
+            color: Colors.blue,
+          ),
           title: const Text('Albums'),
         ),
         ListTile(
           onTap: () => Navigator.popAndPushNamed(context, AboutPage.routeName),
-          leading: const Icon(Icons.info),
+          leading: const Icon(
+            Icons.info,
+            color: Colors.blue,
+          ),
           title: const Text('About'),
         ),
         ListTile(
           onTap: () =>
               Navigator.popAndPushNamed(context, ProductPage.routeName),
-          leading: const Icon(Icons.card_giftcard_outlined),
+          leading: const FaIcon(
+            FontAwesomeIcons.gift,
+            color: Colors.blue,
+          ),
           title: const Text('Gifts'),
         ),
         ListTile(
           onTap: () => Navigator.popAndPushNamed(context, CartScreen.routeName),
-          leading: const Icon(Icons.shopping_cart),
+          leading: const FaIcon(
+            FontAwesomeIcons.cartPlus,
+            color: Colors.blue,
+          ),
           title: const Text('My Gifts'),
         ),
       ],
